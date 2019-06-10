8f9587d8c8fdbce376bf887544069c0fa378ca4f
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-10 17:10:07 +0000

    prints year in which user turns out 95
    Program that asks the user age and prints the year in which they'll turn out 95

---
+++ prog1.py
@@ -0,0 +1,7 @@
+import datetime
+name=input("Enter the name:")
+age=input("Enter age:")
+cur_date=datetime.datetime.now()
+ye=cur_date.year
+tot=95-int(age)
+print("The year in which the person turns out 95 will be:",int(tot)+int(ye))
