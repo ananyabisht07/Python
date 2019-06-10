f75d73fd932c29877d5d4e744d2647153d2c441a
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-10 17:42:35 +0000

    list

---
+++ prog4.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+adhoc=[1,2,3,1,4,5,66,22,2,6,8,9]
+num_gret5=[i for i in adhoc if i>5]
+print("Number greater than 5 :",num_gret5)
+num_less2=[i for i in adhoc if i<=2]
+print("Number is smaller then equal to 2:",num_less2)
