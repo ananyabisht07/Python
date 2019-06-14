486446c2d84c88dac104f65eaaeeeeb3ab7438c4
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-14 21:31:17 +0530

    QRCode generator of top 3 links searched on google

---
+++ qrcode.py
@@ -0,0 +1,14 @@
+import webbrowser
+from googlesearch import search
+import pyqrcode
+from pyqrcode import QRCode
+
+f=open("glinks",'w')
+web =input("ENTER TOPIC TO SEARCH ON GOOGLE :")
+url_list=[]
+for i in search(web,tld="co.in",lang="eng",num=3,start=0,stop=3,pause=2):
+	url_list.append(i)
+	print(i)
+	url=pyqrcode.create(i)
+	url.svg("myqr{}.svg",scale=8)
+webbrowser.open("https://www.google.com/search?q="+web)
