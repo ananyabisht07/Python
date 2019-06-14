33ebf80481dc2bb2c20ba6c767b6fac79f025d5e
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-14 14:27:27 +0000

    Update p8.py

--- p8.py
+++ p8.py
@@ -1,21 +1,18 @@
 import os
 import sys
 from shutil import which
-command=input("Enter command=")
-f=open("comm.txt",'a+')
-f.write(command +"\n")
-print("1")	
+command=input("Enter command=")	
 while(command):
-	for i in f:
-		print("2")	
-		if(which(command)):
+	f=open("comm.txt",'a+')
+	f.write(command +"\n")
+	for i in f:	
+		if command==i:
+			print("Dont do this again")	
+		elif(which(command)):
 			os.system(command)
 			command=input("Enter command=")
 		elif(command=="exit"):
 			sys.exit(0)
-			command=input("Enter command=")
-		elif command==i:
-			print("Dont do this again")	
 			command=input("Enter command=")
 		else:
 			print("Command Not Found....")
