82e1bfbf16da91ec40921e2120523ef15c4fb656
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-15 10:49:26 +0000

    Problem1
    Create a program that asks the user to enter their name and their age.
     Print out a message that will tell them the year that they will turn 95 years old.

--- prog1.py
+++ prog1.py
@@ -4,4 +4,4 @@ age=input("Enter age:")
 cur_date=datetime.datetime.now()
 ye=cur_date.year
 tot=95-int(age)
-print("The year in which the person turns out 95 will be:",int(tot)+int(ye))
+print("The year in which the person turns out 95 will be:",int(tot)+int(ye)) 
