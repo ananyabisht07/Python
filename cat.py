11765ff4fb8e991f55c714c8069435b8c026867f
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-16 15:43:34 +0000

    Update cat.py

--- cat.py
+++ cat.py
@@ -3,7 +3,7 @@ count=0
 f=open("a",'r')
 #Checking for correct command 
 if input1[:3] == "rid" :			
- 	if "-" == input1[4] : 
+ 	if "-" == input1[4] :
 #FOR NUMBER AT STARTING OF EVERY LINE			
 		if 'n' == input1[5] :
 			for i in f :	
@@ -20,14 +20,12 @@ if input1[:3] == "rid" :
 			b.write(i)
 #REDIRECT OUTPUT OF A FILE TO OTHER FILE
 	elif input1[4] == '>' 				
-		print("a")
 		b=open("b",'w')
-		print("b")
 		for i in f :	
-			b.write(i)
+			b.write(i) 
 	else :
 		for i in f :	
 			print(i)
-
-else : 
-	print("INCORRECT COMMAND")
+	
+else :
+print("INCORRECT COMMAND")
