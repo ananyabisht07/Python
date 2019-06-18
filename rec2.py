c9514c7dd9a32dac9f2fbc251230005681a09b20
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-18 08:33:30 +0000

    Update rec2.py

--- rec2.py
+++ rec2.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-
-import socket
+from socket import *
 import sys
+import select
 rec_ip="127.0.0.1"
 rec_port=4444
+s=socket(AF_INET,SOCK_DGRAM)
+s.bind((rec_ip,rec_port))
 
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.connect((rec_ip,rec_port))
-filename=input(str("Please enter the filename for the incoming file:"))
-filee=open(filename,'wb')
-file_data=s.recv(1024)
-filee.write(file_data)
-filee.close()
-s.close()
+addr=(rec_ip,rec_port)
+data,addr=s.recvfrom(1024)
+print("Recieved file:",data.strip())
+filee=open(data.strip(),'wb')
+
+data,addr=s.recvfrom(1024)
+try:
+	while(data):
+		filee.write(data)
+		s.settimeout(2)
+		data,addr=s.recvfrom(1024)
+except timeout:
+	filee.close()
+	s.close()
+	print("file downloaded")
