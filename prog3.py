d0fffe4684849675d113178a426a66a729a516c9
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-19 22:28:58 +0530

    Update prog3.py

--- prog3.py
+++ prog3.py
@@ -1,14 +1,2 @@
-#!usr/bin/python3
-import os
-import crypt
-count=0
-a=input("Enter the input=")
-abc=list(a)
-for i in abc:
-	if i.isdigit():
-		count=1
-if count==0:
-	par="hello"+a
-	encPass=crypt.crypt(par,"22")
-	os.system("useradd -m -p" + encPass +" "+a)
+
  
